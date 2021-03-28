47eb9530a7c66c285a9434e9c0a188df97085020
mattsun <3219808883@qq.com>
2021-03-28 10:15:13 +0800

    更新了process基本使用方法

---
+++ src/main/java/com/sun/state/Test3_FaultTolerance.java
@@ -0,0 +1,61 @@
+package com.sun.state;
+
+import com.sun.models.BatteryData;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @Author Matt Sun
+ * @Date 2021/3/27 7:36 下午
+ * @Version 1.0
+ **/
+public class Test3_FaultTolerance {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //状态后端配置
+        env.setStateBackend(new MemoryStateBackend());
+        env.setStateBackend(new FsStateBackend(""));
+        env.setStateBackend(new RocksDBStateBackend(""));
+
+        //检查点配置，默认是非开启状态
+        env.enableCheckpointing(1000);
+
+        //高级选项
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.AT_LEAST_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60000L);
+
+
+
+        Properties properties = new Properties();
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("properties.properties");
+
+        properties.load(in);
+
+        FlinkKafkaConsumer011 consumer = new FlinkKafkaConsumer011("inputdata",new SimpleStringSchema(), properties);
+
+
+        DataStreamSource<String> inputStream = env.addSource(consumer);
+
+        DataStream<BatteryData> dataStream = inputStream.map(new MapFunction<String, BatteryData>() {
+            @Override
+            public BatteryData map(String value) throws Exception {
+                String[] fields = value.split(",");
+                return new BatteryData(fields[0], Long.valueOf(fields[1]), Double.valueOf(fields[2]),
+                        Double.valueOf(fields[3]), Short.valueOf(fields[4]));
+            }
+        });
+    }
+}
